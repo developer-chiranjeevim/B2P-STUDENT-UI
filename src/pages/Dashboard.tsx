d70135d6ad986a807f49b3dcfc31f2af84021b06
chiranjeevi <developer.chiranjeevim@gmail.com>
2025-12-08 22:56:01 +0530

    Fix: Removed Unused Variable

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
-import { Users, Clock,  Calendar, CheckCircle, Timer, Trash} from 'lucide-react';
+import { Users, Clock,  Calendar, CheckCircle, Timer} from 'lucide-react';
 import {  useNavigate } from "react-router-dom";
 import axios from "axios";
 
